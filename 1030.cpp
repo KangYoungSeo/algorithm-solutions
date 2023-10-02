1b3a09e1395d4fd8a94507f2dc0ab06cdc78f33f
강영서(ZW) <68414594+KangYoungSeo@users.noreply.github.com>
2023-10-02 13:21:16 +0900

    1030 수정

--- 1030.cpp
+++ 1030.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cmath>
 
@@ -12,98 +13,101 @@ int outSizeY, outSizeX;
 //출력 값
 bool board[51][51];
 
-void Recursive(int Nsize, int t, int X, int Y){
-    
-    cout << Nsize << " " << t << " " << X << " " << Y << endl;
-    
-    int Ksize = K * pow(N,S - t);
-    
-    //가운데 계산
-    int middleL = (Nsize - Ksize)/2; // 작은
-    int middleR = (Nsize + Ksize)/2; // 큰
+//처음에 제대로 접근하는 것이 더 중요하다
 
-    cout << Ksize << " " << middleL << " " << middleR << endl;
-    
-    //가운데에 해당하는 칸이 해당 칸에 해당하는지 확인.
-    for(int i = middleL + Y; i < middleR + Y; i++){
-        if(R1 > i || R2 < i) continue;
-        
-        for(int j = middleL + X; j < middleR + X; j++){
-            if(C1 > j || C2 < j) continue;
-            
-            //범위에 해당한다면 색칠하기
-            board[i - R1][j - C1] = true;
-        }
-    }
+void Recursive(int Nsize, int t, int X, int Y) {
 
-    
-    //세분화 종료 시, return 0
-    if(S == t) return;
-    
-    //인덱스 세분화 후에, 분할 정복하기
-    
-    //왼쪽 위
-    if(R1 <= Y + middleL && C1 <= X + middleL){
-        Recursive(Nsize/N, t +1, 0, 0 );
-    }
-    
-    //중간 위
-    if(R1 <= Y + middleL && C1 <= X + middleR && C2 >= X + middleL){
-        
-        cout << "중간 위 탐색" << endl;
-        Recursive(Nsize/N, t +1, X + middleL, 0 );
-    }
-    
-    //오른쪽 위
-    if(C2 >= Y + middleR && X + middleL >= R1){
-        Recursive(Nsize/N, t +1, X + middleR, 0 );
-    }
-    
-    //왼쪽 중간
-    if(C1 <= X + middleL && R1 <= Y + middleR && R2 >= Y + middleL){
-        cout <<"왼쪽 중간 탐색" <<endl;
-        Recursive(Nsize/N, t +1, 0, Y + middleL);
-    }
-    
-    //오른쪽 중간
-    if(C2 >= X + middleR && R1 <= Y + middleR && R2 >= Y + middleL){
-        Recursive(Nsize/N, t +1, X + middleR, Y + middleL);
-    }
-    
-    //왼쪽 아래
-    if(C1 <= X + middleL && R2 >= Y + middleR){
-        Recursive(Nsize/N, t +1, 0, Y + middleR);
-    }
-    
-    //중간 아래
-    if(R2 >= Y + middleR && C1 <= X + middleR && C2 >= X + middleL){
-        Recursive(Nsize/N, t +1, X + middleL, Y + middleR);
-    }
-    
-    //오른쪽 아래
-    if(C2 >= X + middleR && R2 >= Y + middleL){
-        Recursive(Nsize/N, t +1, X + middleR, Y + middleR);
-    }
+	//Nsize 현재 체크하고 있는 나누려고 하는 박스의 크기
+	//t 현재 시간
+	//X, Y 해당 박스가 시작하는 왼쪽 위에 코드
+
+	cout << Nsize << " " << t << " " << X << " " << Y << "탐색중" << endl;
+
+	//Ksize 정사각형의 중간 값의 크기를 scale에 따라서 계산하기
+	int Ksize = K * pow(N, S - t);
+
+	//가운데 계산 =>  해당 Nsize의 정사각형 기준 중간 값의 위, 아래 값 계산
+	int middleL = (Nsize - Ksize) / 2; // 작은
+	int middleR = (Nsize + Ksize) / 2; // 큰
+
+	//cout << Ksize << " " << middleL << " " << middleR << endl;
+
+	//내가 출력하는 부분이
+	//가운데에 해당하는 칸이 해당 칸에 해당하는지 확인.
+	for (int i = middleL + Y; i < middleR + Y; i++) {
+		if (R1 > i || R2 < i) continue;
+
+		for (int j = middleL + X; j < middleR + X; j++) {
+			if (C1 > j || C2 < j) continue;
+
+			//범위에 해당한다면 색칠하기
+			board[i - R1][j - C1] = true;
+		}
+	}
+
+
+	//세분화 종료 시, return 0
+	if (S == t) return;
+
+	//인덱스 세분화 후에, 분할 정복하기
+	//왼쪽 위
+	
+	int NsizeDiv = Nsize/N;
+	
+	//위쪽 라인 탐색
+	
+	if(C1 < X + Nsize && R1 < Y ){
+	    for (int i = Y; i < Y + middleL; i+= NsizeDiv) {
+		    for (int j = X; j < X + Nsize ; j+= NsizeDiv)
+			    Recursive(Nsize / N, t + 1, j, i);
+		}
+	
+	}
+	
+	
+	
+	
+
+	//가운데 탐색
+	for (int i = Y + middleL; i < Y + middleR; i+= NsizeDiv) {
+
+		//왼쪽 탐색
+		for (int j = X; j < X + middleL; j += NsizeDiv ) {
+			Recursive(Nsize / N, t + 1, j, i);
+		}
+
+		//오른쪽 탐색
+		for (int j = X + middleR; j < X + Nsize; j+= NsizeDiv) {
+			Recursive(Nsize / N, t + 1, j, i);
+		}
+	}
+
+	//아래쪽 라인 탐색
+	for (int i = Y + middleR; i < Y + Nsize; i+= NsizeDiv) {
+		for (int j = X; j < X + Nsize; j+= NsizeDiv) {
+			Recursive(Nsize / N, t + 1, j, i);
+		}
+	}
 }
 
-int main(){
-    
-    //input
-    cin >> S >> N >> K >> R1 >> R2 >> C1 >> C2;
-    outSizeY = R2 - R1 + 1;
-    outSizeX = C2 - C1 + 1;
-    MAX = pow(N,S);
-    
-    //Recursive
-    Recursive(MAX,1,0,0);
-    
-    //output
-    for(int i = 0; i < outSizeY ; i++){
-        for(int j = 0; j < outSizeX; j++){
-            cout << board[i][j];   
-        }
-        cout << endl;
-    }
+int main() {
 
-    return 0;
+	//input
+	cin >> S >> N >> K >> R1 >> R2 >> C1 >> C2;
+	outSizeY = R2 - R1 + 1;
+	outSizeX = C2 - C1 + 1;
+	MAX = pow(N, S);
+
+	//Recursive
+	Recursive(MAX, 1, 0, 0);
+
+	//output
+	for (int i = 0; i < outSizeY; i++) {
+		for (int j = 0; j < outSizeX; j++) {
+			cout << board[i][j];
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
